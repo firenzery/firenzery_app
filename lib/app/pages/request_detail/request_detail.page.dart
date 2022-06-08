0b2168b76df217f8fc4dcc6d00588f3ae46d661a
Guilherme Cassiano de Souza <guilherme.cassiano.developer@gmail.com>
2022-06-08 11:44:50 -0300

    adicionado dois campos na tela de detalhes do pedido

--- lib/app/pages/request_detail/request_detail.page.dart
+++ lib/app/pages/request_detail/request_detail.page.dart
@@ -1,5 +1,7 @@
 import 'package:firenzery/app/models/sale.model.dart';
 import 'package:firenzery/app/pages/request_detail/request_detail.controller.dart';
+import 'package:firenzery/app/pages/requests/requests.controller.dart';
+import 'package:firenzery/app/viewmodels/adress.viewmodel.dart';
 import 'package:firenzery/app/viewmodels/sale.viewmodel.dart';
 import 'package:firenzery/constants.dart';
 import 'package:firenzery/size_config.dart';
@@ -19,11 +21,15 @@ class RequestDetailPage extends StatefulWidget {
 class _RequestDetailPageState extends State<RequestDetailPage> {
   late final RequestDetailController controller;
   late final SaleViewModel saleViewModel;
+  late final AdressViewModel adressViewModel;
+
+  final RequestController requestController = RequestController();
 
   @override
   void initState() {
     controller = context.read<RequestDetailController>();
     saleViewModel = Provider.of<SaleViewModel>(context, listen: false);
+    adressViewModel = Provider.of<AdressViewModel>(context, listen: false);
 
     Future.delayed(
         const Duration(milliseconds: 500),
@@ -123,6 +129,76 @@ class _RequestDetailPageState extends State<RequestDetailPage> {
                         )
                       ],
                     ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: defaultPadding),
+              Container(
+                height: getProportionateScreenWidth(75),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(defaultPadding),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Metodo de Pagamento',
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                            maxLines: 2,
+                          ),
+                          SizedBox(height: getProportionateScreenWidth(5)),
+                          Text.rich(
+                            TextSpan(
+                              text: requestController
+                                  .setPayment(widget.sale.payment!),
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: primaryColor),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: defaultPadding),
+              Container(
+                height: getProportionateScreenWidth(75),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(defaultPadding),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Endereço',
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                            maxLines: 2,
+                          ),
+                          SizedBox(height: getProportionateScreenWidth(5)),
+                          Text.rich(
+                            TextSpan(
+                              text:
+                                  "${adressViewModel.adressModel.apartment}${adressViewModel.adressModel.block} GRUPO ${adressViewModel.adressModel.group}",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: primaryColor),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ),
